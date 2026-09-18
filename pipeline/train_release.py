8131e4578ca645314c3e49bf3b6e7d2450f54401
Martan <martan@spinque.com>
2026-09-18 23:54:12 +0200

    Fix training entry point imports

--- pipeline/train_release.py
+++ pipeline/train_release.py
@@ -13,6 +13,7 @@ import pandas as pd
 import torch
 
 sys.path.insert(0, str(Path(__file__).parent))
+sys.path.insert(0, str(Path(__file__).parent.parent))
 from finetuner.data import load_data, build_dataloaders
 from finetuner.model import get_device, load_tokenizer, train_model, evaluate_model, save_model
 from finetuner.data import build_input
